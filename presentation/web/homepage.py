a8f695c714507021ca7ba9a61bfae72fc7d492a2
Leo <liuysh20@gmail.com>
2023-12-02 22:39:43 +0800

    feat：添加等待status

--- presentation/web/homepage.py
+++ presentation/web/homepage.py
@@ -18,18 +18,19 @@ class Streamlit:
 
         st.link_button("B站主页", "https://space.bilibili.com/244791824")
 
-        label_upload = "在下方上传你和native speaker的对话视频或者录音,耐心等待一段时间后，会出现下载按钮，你将得到GPT给出的地道表达"
+        label_upload = "在下方上传你和native speaker的对话视频或者录音,得到GPT给出的地道表达建议"
         uploaded_file = st.file_uploader(label_upload, type=["mp4", "mp3"])
-        url = None
+
         if uploaded_file is not None:
-            url = MainProcessor().execute(uploaded_file)
+            #  等待状态
+            url = None
+            with st.spinner('Wait for it... 步骤处理比较多，可能需要耐心等几分钟'):
+                url = MainProcessor().execute(uploaded_file)
+                label_download = "点击下载（这是GPT帮你纠正后的地道表达）"
+                if url is not None:
+                    with open(url , "rb") as result:
+                        btn = st.download_button(label_download, result, file_name=url, mime=None, key=None, help=None, on_click=None, args=None,
+                                           kwargs=None, type="secondary", disabled=False, use_container_width=False)
 
-        label_download = "这是GPT帮你纠正后的地道表达"
-        if url is not None:
-            with open(url , "rb") as result:
-                btn = st.download_button(label_download, result, file_name=url, mime=None, key=None, help=None, on_click=None, args=None,
-                                   kwargs=None, type="secondary", disabled=False, use_container_width=False)
-
-
-
+                st.success('Done!')
 
