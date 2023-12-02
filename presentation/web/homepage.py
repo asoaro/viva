0491fcb7c483071700e3be4303703a5cf82096b0
Leo <liuysh20@gmail.com>
2023-12-02 22:28:32 +0800

    feat：更新文本

--- presentation/web/homepage.py
+++ presentation/web/homepage.py
@@ -11,16 +11,14 @@ from domain.services.video_processor import VideoProcessor
 class Streamlit:
     def display(self):
         body = """
-        相信所有的进步，都来自于尝试、反馈、间隔复习
-        相信所有的自律，都来自于热爱，追求，和坚持！
+        相信所有的进步，都来自于尝试、反馈、间隔复习！
         """
         st.title("viva")
-        st.image("avatar.png", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
         st.markdown(body, unsafe_allow_html=False, help=None)
 
         st.link_button("B站主页", "https://space.bilibili.com/244791824")
 
-        label_upload = "在下方上传你和native speaker的对话视频或者录音"
+        label_upload = "在下方上传你和native speaker的对话视频或者录音,耐心等待一段时间后，会出现下载按钮，你将得到GPT给出的地道表达"
         uploaded_file = st.file_uploader(label_upload, type=["mp4", "mp3"])
         url = None
         if uploaded_file is not None:
