6f8ce7c719650f792814a460b7c15509660c5bb9
Leo <liuysh20@gmail.com>
2023-12-03 15:20:58 +0800

    feat：限制说话角色为2人

--- infrastructure/app_clients/assemblyai_client.py
+++ infrastructure/app_clients/assemblyai_client.py
@@ -8,5 +8,6 @@ class AssemblyAIClient:
         aai.settings.api_key = os.getenv('ASSEMBLYAI_API_KEY')
         config = aai.TranscriptionConfig(
             speaker_labels=True,
+            speakers_expected=2,
         )
         return aai.Transcriber(config=config)
